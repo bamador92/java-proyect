ec19838c644667f2a8afe40c6b8f7eb233fffbc4
bamador92 <braian.amador@gmail.com>
2021-12-28 11:44:07 -0300

    Primer commit

---
+++ src/Empleado.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Empleado {
+       private Scanner teclado;
+       private String apellido;
+       private String Codigo;
+       private int hsTrab;
+
+       public void inicializador() {
+           teclado = new Scanner(System.in);
+           System.out.println("ingrese el nombre del empleado: ");
+
+
+
+
+       }
+    }
