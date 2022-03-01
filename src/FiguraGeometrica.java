5d80051e8cefd6fb62b57c4ce5ee7663a29bda0f
BraianAmador <31548855+BraianUTN@users.noreply.github.com>
2022-02-28 21:58:38 -0300

    Pag86

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -5,12 +5,23 @@ public abstract class FiguraGeometrica {
     //metodo abstracto
     public abstract double area();
 
+    public static double prom(FiguraGeometrica arr[])
+    {
+        int sum = 0;
+        for(int i=0; i<arr.length; i++)
+        {
+            sum += arr[i].area();
+        }
+        return sum / arr.length;
+    }
+
     //agregamos un constructor
     public FiguraGeometrica(String nom)
     {
         nombre = nom;
     }
 
+    //ahora en el toString tambien mostramos el nombre
     public String toString() {
         return nombre + " (area = " + area()+") ";
     }
--- src/TestFigurasGeometricas.java
+++ src/TestFigurasGeometricas.java
@@ -6,5 +6,12 @@ public class TestFigurasGeometricas {
         System.out.println(circulo);
         Triangulo triangulo = new Triangulo(8,2);
         System.out.println(triangulo);
+
+        FiguraGeometrica arr[] = { new Circulo(23)
+                ,new Rectangulo(12,4)
+                ,new Triangulo(2,5)};
+
+        double prom = FiguraGeometrica.prom(arr);
+        System.out.println("Promedio = " + prom);
     }
 }
